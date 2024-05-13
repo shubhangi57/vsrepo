36ab2bc73cd6f593a0578b951d3b2d95bb3316c4
shubhangi57 <shubhangishelke18@gmail.com>
2024-05-13 16:15:12 +0530

    ...

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 provider "aws" {
- region = "us-east-1"
+   region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
- ami = "ami-07caf09b362be10b8"
+ ami           = "ami-07caf09b362be10b8"
  instance_type = "t2.micro"
 }
+
